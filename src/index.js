5ab8b70794f334eea77464e97948e1db8de31d5b
asmitaupreti <asmitaupreti52@gmail.com>
2024-02-27 14:35:33 +0100

    connect to mongoatlas

--- src/index.js
+++ src/index.js
@@ -1,13 +1,34 @@
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+connectDB();
+/** 
 import express from "express";
-import "dotenv/config";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants";
 
 const app = express();
 const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
 
-app.listen(port, () => {
-  console.log(`Example app is listing on port ${port}`);
-});
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.log("ERROR", error);
+      throw error;
+    });
+    app.listen(port, () => {
+      console.log(`Example app is listing on port ${port}`);
+    });
+  } catch (error) {
+    console.log("ERROR", error);
+    throw error;
+  }
+})();
+*/
