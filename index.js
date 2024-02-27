7c4b1a6a1772c12603bd7d65bdb201d4fa3f19f6
asmitaupreti <asmitaupreti52@gmail.com>
2024-02-27 13:27:11 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import "dotenv/config";
+
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello world");
+});
+
+app.listen(port, () => {
+  console.log(`Example app is listing on port ${port}`);
+});
